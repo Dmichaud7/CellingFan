03205b924c19c80178bb8ddc2bc513294d306024
Dmichaud7 <59565701+Dmichaud7@users.noreply.github.com>
2021-04-19 16:41:39 -0300

    init

---
+++ Fan/Fan/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Fan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnFanSpeed = new System.Windows.Forms.Button();
+            this.btnRotation = new System.Windows.Forms.Button();
+            this.lblFan = new System.Windows.Forms.Label();
+            this.picFan = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picFan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFanSpeed
+            // 
+            this.btnFanSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFanSpeed.Location = new System.Drawing.Point(307, 257);
+            this.btnFanSpeed.Name = "btnFanSpeed";
+            this.btnFanSpeed.Size = new System.Drawing.Size(146, 35);
+            this.btnFanSpeed.TabIndex = 0;
+            this.btnFanSpeed.Text = "Speed";
+            this.btnFanSpeed.UseVisualStyleBackColor = true;
+            this.btnFanSpeed.Click += new System.EventHandler(this.btnFanSpeed_Click);
+            // 
+            // btnRotation
+            // 
+            this.btnRotation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRotation.Location = new System.Drawing.Point(88, 257);
+            this.btnRotation.Name = "btnRotation";
+            this.btnRotation.Size = new System.Drawing.Size(146, 35);
+            this.btnRotation.TabIndex = 1;
+            this.btnRotation.Text = "Rotation";
+            this.btnRotation.UseVisualStyleBackColor = true;
+            this.btnRotation.Click += new System.EventHandler(this.btnRotation_Click);
+            // 
+            // lblFan
+            // 
+            this.lblFan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFan.Location = new System.Drawing.Point(12, 212);
+            this.lblFan.Name = "lblFan";
+            this.lblFan.Size = new System.Drawing.Size(535, 24);
+            this.lblFan.TabIndex = 2;
+            this.lblFan.Text = "fan info";
+            this.lblFan.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picFan
+            // 
+            this.picFan.Image = global::Fan.Properties.Resources.fan;
+            this.picFan.Location = new System.Drawing.Point(167, 27);
+            this.picFan.Name = "picFan";
+            this.picFan.Size = new System.Drawing.Size(221, 171);
+            this.picFan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picFan.TabIndex = 3;
+            this.picFan.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(559, 319);
+            this.Controls.Add(this.picFan);
+            this.Controls.Add(this.lblFan);
+            this.Controls.Add(this.btnRotation);
+            this.Controls.Add(this.btnFanSpeed);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Celling Fan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picFan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFanSpeed;
+        private System.Windows.Forms.Button btnRotation;
+        private System.Windows.Forms.Label lblFan;
+        private System.Windows.Forms.PictureBox picFan;
+    }
+}
+
---
+++ Fan/Fan/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fan
+{
+    //Denis Michaud
+    //This represent my own work.
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int speed = 0;
+        string fanRotation = "going clockwise.";
+        bool flag = false;
+
+        /// <summary>
+        /// Loads the default information for the fan
+        /// fan currently at speed 0 (off)
+        /// and fan rotation set to clockwise
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                FanInfo();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
+
+        /// <summary>
+        /// Change the rotation of the celling fan
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRotation_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                flag = !flag;
+
+                if (flag)
+                {
+                    fanRotation = "going counter clockwise.";
+                
+                    FanInfo();
+                }
+                else
+                {
+                    fanRotation = "going clockwise.";
+                    FanInfo();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
+
+        /// <summary>
+        /// Change the speed of the celling fan
+        /// if the fan speed is 0 the fan is currently off
+        /// if the fan speed goes pass 3 it goes back to 0 (off)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFanSpeed_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                speed++;
+
+                switch (speed)
+                {
+                    case 1:
+                        FanInfo();
+                        break;
+                    case 2:
+                        FanInfo();
+                        break;
+                    case 3:
+                        FanInfo();
+                        break;
+                    default:
+                        speed = 0;
+                        FanInfo();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
+
+        /// <summary>
+        /// Method to display the current state of the celling fan
+        /// and to change the image display depending on the current state of the celling fan (off, clockwise, counter clockwise)
+        /// </summary>
+        private void FanInfo()
+        {
+            if(speed > 0)
+            {
+                lblFan.Text = "Celling fan is currently at speed " + speed + " " + fanRotation;
+
+                if (flag)
+                {
+                    picFan.Image = Fan.Properties.Resources.counterclockwise;
+                }
+                else
+                {
+                    picFan.Image = Fan.Properties.Resources.clockwise;
+                }
+               
+            }
+            else
+            {
+                lblFan.Text = "Celling fan is currently off.";
+                picFan.Image = Fan.Properties.Resources.fan;
+            }
+       
+        }
+
+ 
+    }
+}
